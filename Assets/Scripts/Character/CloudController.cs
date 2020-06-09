aebedc6e94abdeafda15d4d3d4f2a1888514d08f
sh3b0 <ahmedshaban784@gmail.com>
2020-06-09 11:36:17 +0300

    Added the switching between player and cloud using "Tab" button
    Added a cloud gameObject and a script CloudController
    Modified Controller2D to switch on "Tab" key press
    Removed some lights and backwall (Let's keep it simple for now and add them later when needed)

---
+++ Assets/Scripts/Character/CloudController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CloudController : MonoBehaviour
+{
+    public float moveSpeed;
+    void FixedUpdate()
+    {
+        if (Controller2D.cloudActive)
+        {
+            transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal") * moveSpeed, transform.position.y);
+        }
+    }
+}
