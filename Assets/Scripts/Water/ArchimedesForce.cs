3cc17305c9199a7bc7459f288ae77e62fd9c3df2
BoogiemanNSK <calah47@yandex.ru>
2020-06-10 17:19:00 +0300

    Water fills gap, and pushes objects upwards

---
+++ Assets/Scripts/Water/ArchimedesForce.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using UnityEngine;
+
+public class ArchimedesForce : MonoBehaviour {
+
+    public float Force;
+
+    private Collection<Rigidbody> _rigidbodiesInWater;
+
+    private void Start() {
+        _rigidbodiesInWater = new Collection<Rigidbody>();
+    }
+
+    private void Update() {
+        foreach (var thisRigidbody in _rigidbodiesInWater) {
+            thisRigidbody.AddForce(0, Force, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Floating")) {
+            print("Box entered");
+            _rigidbodiesInWater.Add(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.CompareTag("Floating")) {
+            _rigidbodiesInWater.Remove(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+}
---
+++ Assets/Scripts/Water/WaterRise.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WaterRise : MonoBehaviour {
+
+    public Transform FullWaterObject;
+    public float MinLocalHeight;
+    public float MaxLocalHeight;
+    public float IncreaseDelta;
+    
+    private void Start() {
+        var oldPos = FullWaterObject.localPosition;
+        FullWaterObject.localPosition = new Vector3(oldPos.x, MinLocalHeight, oldPos.z);
+    }
+
+    // Call this method to increase water level for a bit
+    public void IncreaseWaterlevel() {
+        var oldPos = FullWaterObject.localPosition;
+        var newHeight = oldPos.y + IncreaseDelta;
+        if (newHeight >= MaxLocalHeight) { return; }
+        FullWaterObject.localPosition = new Vector3(oldPos.x, newHeight, oldPos.z);
+    }
+    
+}
