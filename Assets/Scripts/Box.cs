fbd2784760355bc98c2e656c167f94e421cec0de
sh3b0 <ahmedshaban784@gmail.com>
2020-06-10 11:31:58 +0300

    Fixed some bugs: 	The cloud going outside the scene 	the box moving while controlling the cloud
    Edited the camera script:
    	Still needs changes before using.
    
    Some minor edits and variables renaming.

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -5,10 +5,10 @@ public class Box : MonoBehaviour
     public float speed;
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && Player.playerActive)
         {
             print("Now holding the box"); // Some animation of the player holding the box
-            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0), ForceMode.Acceleration);
+            GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal") * speed, 0, 0));
             // transform.position = new Vector3(transform.position.x + Input.GetAxis("Horizontal"), transform.position.y, transform.position.z);
         }
     }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,15 @@ public class Player : MonoBehaviour
 {
     public float MoveSpeed, JumpForce, GravityScale;
 
-    private CharacterController _playerPhysics;
-    private Vector3 _moveDirection;
-    private int _jumpsCount;
-
+    private CharacterController playerPhysics;
+    private Vector3 moveDirection;
+    private int jumpsCount;
     public static bool playerActive = true, cloudActive = false;
 
     private void Start()
     {
-        _playerPhysics = GetComponent<CharacterController>();
-        _jumpsCount = 0;
+        playerPhysics = GetComponent<CharacterController>();
+        jumpsCount = 0;
     }
 
     private void Update()
@@ -28,28 +27,28 @@ public class Player : MonoBehaviour
             cloudActive = !cloudActive;
         }
 
-        if (playerActive || !_playerPhysics.isGrounded)
+        if (playerActive || !playerPhysics.isGrounded)
         {
-            _moveDirection = new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, _moveDirection.y);
+            moveDirection = new Vector3(Input.GetAxis("Horizontal") * MoveSpeed, moveDirection.y);
 
-            if (_playerPhysics.isGrounded)
+            if (playerPhysics.isGrounded)
             {
-                _jumpsCount = 0;
+                jumpsCount = 0;
             }
 
             if (Input.GetAxis("Vertical") > 0 && Input.GetButtonDown("Vertical"))
             {
-                if (_jumpsCount < 2)
+                if (jumpsCount < 2)
                 {
-                    _moveDirection = new Vector3(_playerPhysics.velocity.x, JumpForce);
-                    _jumpsCount++;
+                    moveDirection = new Vector3(playerPhysics.velocity.x, JumpForce);
+                    jumpsCount++;
                 }
             }
 
-            _moveDirection.y += Physics.gravity.y * GravityScale * Time.deltaTime;
+            moveDirection.y += Physics.gravity.y * GravityScale * Time.deltaTime;
 
-            if (!_playerPhysics.enabled) { return; }
-            _playerPhysics.Move(_moveDirection * Time.deltaTime);
+            if (!playerPhysics.enabled) { return; }
+            playerPhysics.Move(moveDirection * Time.deltaTime);
         }
     }
 
@@ -59,11 +58,12 @@ public class Player : MonoBehaviour
         // TODO Freeze Controller when game ends
         //if (GameState.IsGameFinished()) return;
 
-        _playerPhysics.enabled = false;
+        playerPhysics.enabled = false;
+        playerActive = false;
 
         var rb = gameObject.AddComponent<Rigidbody>();
 
-        if (_moveDirection.x >= 0)
+        if (moveDirection.x >= 0)
         {
             rb.AddForce(Vector3.right * 2, ForceMode.Impulse);
         }
